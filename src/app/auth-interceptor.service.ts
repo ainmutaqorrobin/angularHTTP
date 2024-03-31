b1ec0455f7c5978d92fbfea3c41f11850a035fe0
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-03-31 15:49:47 +0800

    using multiple interceptors

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -14,22 +14,10 @@ export class AuthInterceptorService implements HttpInterceptor {
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    console.log(`Request is on its way`);
-    console.log(req.url);
-
     const modifiedHeader = req.clone({
       headers: req.headers.append('Auth', 'robin interceptor header'),
     });
 
-    return next.handle(modifiedHeader).pipe(
-      tap((event) => {
-        console.log(event);
-
-        if (event.type === HttpEventType.Response) {
-          console.log(`response arrived`);
-          console.log(event.body);
-        }
-      })
-    );
+    return next.handle(modifiedHeader);
   }
 }
