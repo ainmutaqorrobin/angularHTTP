5a51f18771a6b38c55cf8c47cceb1ecad682bae1
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-03-31 15:32:59 +0800

    working with response using interceptors

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,10 +1,13 @@
 import {
   HttpEvent,
+  HttpEventType,
   HttpHandler,
   HttpInterceptor,
   HttpRequest,
+  HttpResponse,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(
@@ -17,6 +20,16 @@ export class AuthInterceptorService implements HttpInterceptor {
     const modifiedHeader = req.clone({
       headers: req.headers.append('Auth', 'robin interceptor header'),
     });
-    return next.handle(modifiedHeader);
+
+    return next.handle(modifiedHeader).pipe(
+      tap((event) => {
+        console.log(event);
+
+        if (event.type === HttpEventType.Response) {
+          console.log(`response arrived`);
+          console.log(event.body);
+        }
+      })
+    );
   }
 }
